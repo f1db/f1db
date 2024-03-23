379c067189219cade596d77c9375e7df97503645
Marcel Overdijk <marceloverdijk@gmail.com>
2024-03-23 20:59:29 +0100

    Updated column size of sql dump files

--- buildSrc/src/main/resources/sql/create_schema.sql
+++ buildSrc/src/main/resources/sql/create_schema.sql
@@ -1,20 +1,20 @@
 CREATE TABLE "continent"
-( "id" VARCHAR(255) NOT NULL
+( "id" VARCHAR(100) NOT NULL
 , "code" VARCHAR(2) NOT NULL
-, "name" VARCHAR(255) NOT NULL
-, "demonym" VARCHAR(255)
+, "name" VARCHAR(100) NOT NULL
+, "demonym" VARCHAR(100)
 , CONSTRAINT "cont_pk" PRIMARY KEY ("id")
 , CONSTRAINT "cont_code_uk" UNIQUE ("code")
 , CONSTRAINT "cont_name_uk" UNIQUE ("name")
 );
 
 CREATE TABLE "country"
-( "id" VARCHAR(255) NOT NULL
+( "id" VARCHAR(100) NOT NULL
 , "alpha2_code" VARCHAR(2) NOT NULL
 , "alpha3_code" VARCHAR(3) NOT NULL
-, "name" VARCHAR(255) NOT NULL
-, "demonym" VARCHAR(255)
-, "continent_id" VARCHAR(255) NOT NULL
+, "name" VARCHAR(100) NOT NULL
+, "demonym" VARCHAR(100)
+, "continent_id" VARCHAR(100) NOT NULL
 , CONSTRAINT "coun_pk" PRIMARY KEY ("id")
 , CONSTRAINT "coun_alpha2_code_uk" UNIQUE ("alpha2_code")
 , CONSTRAINT "coun_alpha3_code_uk" UNIQUE ("alpha3_code")
@@ -25,20 +25,20 @@ CREATE TABLE "country"
 CREATE INDEX "coun_continent_id_idx" ON "country" ("continent_id");
 
 CREATE TABLE "driver"
-( "id" VARCHAR(255) NOT NULL
-, "name" VARCHAR(255) NOT NULL
-, "first_name" VARCHAR(255) NOT NULL
-, "last_name" VARCHAR(255) NOT NULL
-, "full_name" VARCHAR(255) NOT NULL
+( "id" VARCHAR(100) NOT NULL
+, "name" VARCHAR(100) NOT NULL
+, "first_name" VARCHAR(100) NOT NULL
+, "last_name" VARCHAR(100) NOT NULL
+, "full_name" VARCHAR(100) NOT NULL
 , "abbreviation" VARCHAR(3) NOT NULL
 , "permanent_number" VARCHAR(2)
-, "gender" VARCHAR(255) NOT NULL
+, "gender" VARCHAR(6) NOT NULL
 , "date_of_birth" DATE NOT NULL
 , "date_of_death" DATE
-, "place_of_birth" VARCHAR(255) NOT NULL
-, "country_of_birth_country_id" VARCHAR(255) NOT NULL
-, "nationality_country_id" VARCHAR(255) NOT NULL
-, "second_nationality_country_id" VARCHAR(255)
+, "place_of_birth" VARCHAR(100) NOT NULL
+, "country_of_birth_country_id" VARCHAR(100) NOT NULL
+, "nationality_country_id" VARCHAR(100) NOT NULL
+, "second_nationality_country_id" VARCHAR(100)
 , "best_championship_position" INTEGER
 , "best_race_result" INTEGER
 , "best_starting_grid_position" INTEGER
@@ -75,9 +75,9 @@ CREATE INDEX "driv_nationality_country_id_idx" ON "driver" ("nationality_country
 CREATE INDEX "driv_second_nationality_country_id_idx" ON "driver" ("second_nationality_country_id");
 
 CREATE TABLE "driver_family_relationship"
-( "driver_id" VARCHAR(255) NOT NULL
-, "other_driver_id" VARCHAR(255) NOT NULL
-, "type" VARCHAR(255) NOT NULL
+( "driver_id" VARCHAR(100) NOT NULL
+, "other_driver_id" VARCHAR(100) NOT NULL
+, "type" VARCHAR(50) NOT NULL
 , CONSTRAINT "drfr_pk" PRIMARY KEY ("driver_id", "other_driver_id", "type")
 , CONSTRAINT "drfr_driver_id_fk" FOREIGN KEY ("driver_id") REFERENCES "driver" ("id")
 , CONSTRAINT "drfr_other_driver_id_fk" FOREIGN KEY ("other_driver_id") REFERENCES "driver" ("id")
@@ -87,10 +87,10 @@ CREATE INDEX "drfr_driver_id_idx" ON "driver_family_relationship" ("driver_id");
 CREATE INDEX "drfr_other_driver_id_idx" ON "driver_family_relationship" ("other_driver_id");
 
 CREATE TABLE "constructor"
-( "id" VARCHAR(255) NOT NULL
-, "name" VARCHAR(255) NOT NULL
-, "full_name" VARCHAR(255) NOT NULL
-, "country_id" VARCHAR(255) NOT NULL
+( "id" VARCHAR(100) NOT NULL
+, "name" VARCHAR(100) NOT NULL
+, "full_name" VARCHAR(100) NOT NULL
+, "country_id" VARCHAR(100) NOT NULL
 , "best_championship_position" INTEGER
 , "best_race_result" INTEGER
 , "best_starting_grid_position" INTEGER
@@ -114,8 +114,8 @@ CREATE INDEX "cons_full_name_idx" ON "constructor" ("full_name");
 CREATE INDEX "cons_country_id_idx" ON "constructor" ("country_id");
 
 CREATE TABLE "constructor_previous_next_constructor"
-( "constructor_id" VARCHAR(255) NOT NULL
-, "previous_next_constructor_id" VARCHAR(255) NOT NULL
+( "constructor_id" VARCHAR(100) NOT NULL
+, "previous_next_constructor_id" VARCHAR(100) NOT NULL
 , "year_from" INTEGER NOT NULL
 , "year_to" INTEGER
 , CONSTRAINT "cpnc_pk" PRIMARY KEY ("constructor_id", "previous_next_constructor_id", "year_from")
@@ -127,9 +127,9 @@ CREATE INDEX "cpnc_constructor_id_idx" ON "constructor_previous_next_constructor
 CREATE INDEX "cpnc_previous_next_constructor_id_idx" ON "constructor_previous_next_constructor" ("previous_next_constructor_id");
 
 CREATE TABLE "engine_manufacturer"
-( "id" VARCHAR(255) NOT NULL
-, "name" VARCHAR(255) NOT NULL
-, "country_id" VARCHAR(255) NOT NULL
+( "id" VARCHAR(100) NOT NULL
+, "name" VARCHAR(100) NOT NULL
+, "country_id" VARCHAR(100) NOT NULL
 , "best_championship_position" INTEGER
 , "best_race_result" INTEGER
 , "best_starting_grid_position" INTEGER
@@ -151,9 +151,9 @@ CREATE INDEX "enma_name_idx" ON "engine_manufacturer" ("name");
 CREATE INDEX "enma_country_id_idx" ON "engine_manufacturer" ("country_id");
 
 CREATE TABLE "tyre_manufacturer"
-( "id" VARCHAR(255) NOT NULL
-, "name" VARCHAR(255) NOT NULL
-, "country_id" VARCHAR(255) NOT NULL
+( "id" VARCHAR(100) NOT NULL
+, "name" VARCHAR(100) NOT NULL
+, "country_id" VARCHAR(100) NOT NULL
 , "best_race_result" INTEGER
 , "best_starting_grid_position" INTEGER
 , "total_race_entries" INTEGER NOT NULL
@@ -172,21 +172,21 @@ CREATE INDEX "tyma_name_idx" ON "tyre_manufacturer" ("name");
 CREATE INDEX "tyma_country_id_idx" ON "tyre_manufacturer" ("country_id");
 
 CREATE TABLE "entrant"
-( "id" VARCHAR(255) NOT NULL
-, "name" VARCHAR(255) NOT NULL
+( "id" VARCHAR(100) NOT NULL
+, "name" VARCHAR(100) NOT NULL
 , CONSTRAINT "entr_pk" PRIMARY KEY ("id")
 );
 
 CREATE INDEX "entr_name_idx" ON "entrant" ("name");
 
 CREATE TABLE "circuit"
-( "id" VARCHAR(255) NOT NULL
-, "name" VARCHAR(255) NOT NULL
-, "full_name" VARCHAR(255) NOT NULL
+( "id" VARCHAR(100) NOT NULL
+, "name" VARCHAR(100) NOT NULL
+, "full_name" VARCHAR(100) NOT NULL
 , "previous_names" VARCHAR(255)
-, "type" VARCHAR(255) NOT NULL
-, "place_name" VARCHAR(255) NOT NULL
-, "country_id" VARCHAR(255) NOT NULL
+, "type" VARCHAR(6) NOT NULL
+, "place_name" VARCHAR(100) NOT NULL
+, "country_id" VARCHAR(100) NOT NULL
 , "latitude" DECIMAL(10,6)
 , "longitude" DECIMAL(10,6)
 , "total_races_held" INTEGER NOT NULL
@@ -201,12 +201,12 @@ CREATE INDEX "circ_place_name_idx" ON "circuit" ("place_name");
 CREATE INDEX "circ_country_id_idx" ON "circuit" ("country_id");
 
 CREATE TABLE "grand_prix"
-( "id" VARCHAR(255) NOT NULL
-, "name" VARCHAR(255) NOT NULL
-, "full_name" VARCHAR(255) NOT NULL
-, "short_name" VARCHAR(255) NOT NULL
+( "id" VARCHAR(100) NOT NULL
+, "name" VARCHAR(100) NOT NULL
+, "full_name" VARCHAR(100) NOT NULL
+, "short_name" VARCHAR(100) NOT NULL
 , "abbreviation" VARCHAR(3) NOT NULL
-, "country_id" VARCHAR(255)
+, "country_id" VARCHAR(100)
 , "total_races_held" INTEGER NOT NULL
 , CONSTRAINT "grpr_pk" PRIMARY KEY ("id")
 , CONSTRAINT "grpr_country_id_fk" FOREIGN KEY ("country_id") REFERENCES "country" ("id")
@@ -225,8 +225,8 @@ CREATE TABLE "season"
 
 CREATE TABLE "season_entrant"
 ( "year" INTEGER NOT NULL
-, "entrant_id" VARCHAR(255) NOT NULL
-, "country_id" VARCHAR(255) NOT NULL
+, "entrant_id" VARCHAR(100) NOT NULL
+, "country_id" VARCHAR(100) NOT NULL
 , CONSTRAINT "seen_pk" PRIMARY KEY ("year", "entrant_id")
 , CONSTRAINT "seen_year_fk" FOREIGN KEY ("year") REFERENCES "season" ("year")
 , CONSTRAINT "seen_entrant_id_fk" FOREIGN KEY ("entrant_id") REFERENCES "entrant" ("id")
@@ -239,9 +239,9 @@ CREATE INDEX "seen_country_id_idx" ON "season_entrant" ("country_id");
 
 CREATE TABLE "season_entrant_constructor"
 ( "year" INTEGER NOT NULL
-, "entrant_id" VARCHAR(255) NOT NULL
-, "constructor_id" VARCHAR(255) NOT NULL
-, "engine_manufacturer_id" VARCHAR(255) NOT NULL
+, "entrant_id" VARCHAR(100) NOT NULL
+, "constructor_id" VARCHAR(100) NOT NULL
+, "engine_manufacturer_id" VARCHAR(100) NOT NULL
 , CONSTRAINT "seec_pk" PRIMARY KEY ("year", "entrant_id", "constructor_id", "engine_manufacturer_id")
 , CONSTRAINT "seec_year_fk" FOREIGN KEY ("year") REFERENCES "season" ("year")
 , CONSTRAINT "seec_entrant_id_fk" FOREIGN KEY ("entrant_id") REFERENCES "entrant" ("id")
@@ -256,10 +256,10 @@ CREATE INDEX "seec_engine_manufacturer_id_idx" ON "season_entrant_constructor" (
 
 CREATE TABLE "season_entrant_tyre_manufacturer"
 ( "year" INTEGER NOT NULL
-, "entrant_id" VARCHAR(255) NOT NULL
-, "constructor_id" VARCHAR(255) NOT NULL
-, "engine_manufacturer_id" VARCHAR(255) NOT NULL
-, "tyre_manufacturer_id" VARCHAR(255) NOT NULL
+, "entrant_id" VARCHAR(100) NOT NULL
+, "constructor_id" VARCHAR(100) NOT NULL
+, "engine_manufacturer_id" VARCHAR(100) NOT NULL
+, "tyre_manufacturer_id" VARCHAR(100) NOT NULL
 , CONSTRAINT "setm_pk" PRIMARY KEY ("year", "entrant_id", "constructor_id", "engine_manufacturer_id", "tyre_manufacturer_id")
 , CONSTRAINT "setm_year_fk" FOREIGN KEY ("year") REFERENCES "season" ("year")
 , CONSTRAINT "setm_entrant_id_fk" FOREIGN KEY ("entrant_id") REFERENCES "entrant" ("id")
@@ -276,12 +276,12 @@ CREATE INDEX "setm_tyre_manufacturer_id_idx" ON "season_entrant_tyre_manufacture
 
 CREATE TABLE "season_entrant_driver"
 ( "year" INTEGER NOT NULL
-, "entrant_id" VARCHAR(255) NOT NULL
-, "constructor_id" VARCHAR(255) NOT NULL
-, "engine_manufacturer_id" VARCHAR(255) NOT NULL
-, "driver_id" VARCHAR(255) NOT NULL
-, "rounds" VARCHAR(255)
-, "rounds_text" VARCHAR(255)
+, "entrant_id" VARCHAR(100) NOT NULL
+, "constructor_id" VARCHAR(100) NOT NULL
+, "engine_manufacturer_id" VARCHAR(100) NOT NULL
+, "driver_id" VARCHAR(100) NOT NULL
+, "rounds" VARCHAR(100)
+, "rounds_text" VARCHAR(100)
 , "test_driver" BOOLEAN NOT NULL
 , CONSTRAINT "seed_pk" PRIMARY KEY ("year", "entrant_id", "constructor_id", "engine_manufacturer_id", "driver_id")
 , CONSTRAINT "seed_year_fk" FOREIGN KEY ("year") REFERENCES "season" ("year")
@@ -301,8 +301,8 @@ CREATE TABLE "season_driver_standing"
 ( "year" INTEGER NOT NULL
 , "position_display_order" INTEGER NOT NULL
 , "position_number" INTEGER
-, "position_text" VARCHAR(255) NOT NULL
-, "driver_id" VARCHAR(255) NOT NULL
+, "position_text" VARCHAR(4) NOT NULL
+, "driver_id" VARCHAR(100) NOT NULL
 , "points" DECIMAL(8,2) NOT NULL
 , CONSTRAINT "seds_pk" PRIMARY KEY ("year", "position_display_order")
 , CONSTRAINT "seds_year_fk" FOREIGN KEY ("year") REFERENCES "season" ("year")
@@ -319,9 +319,9 @@ CREATE TABLE "season_constructor_standing"
 ( "year" INTEGER NOT NULL
 , "position_display_order" INTEGER NOT NULL
 , "position_number" INTEGER
-, "position_text" VARCHAR(255) NOT NULL
-, "constructor_id" VARCHAR(255) NOT NULL
-, "engine_manufacturer_id" VARCHAR(255) NOT NULL
+, "position_text" VARCHAR(4) NOT NULL
+, "constructor_id" VARCHAR(100) NOT NULL
+, "engine_manufacturer_id" VARCHAR(100) NOT NULL
 , "points" DECIMAL(8,2) NOT NULL
 , CONSTRAINT "secs_pk" PRIMARY KEY ("year", "position_display_order")
 , CONSTRAINT "secs_year_fk" FOREIGN KEY ("year") REFERENCES "season" ("year")
@@ -342,39 +342,39 @@ CREATE TABLE "race"
 , "round" INTEGER NOT NULL
 , "date" DATE NOT NULL
 , "time" TEXT
-, "grand_prix_id" VARCHAR(255) NOT NULL
-, "official_name" VARCHAR(255) NOT NULL
-, "qualifying_format" VARCHAR(255) NOT NULL
-, "sprint_qualifying_format" VARCHAR(255)
-, "circuit_id" VARCHAR(255) NOT NULL
-, "circuit_type" VARCHAR(255) NOT NULL
+, "grand_prix_id" VARCHAR(100) NOT NULL
+, "official_name" VARCHAR(100) NOT NULL
+, "qualifying_format" VARCHAR(20) NOT NULL
+, "sprint_qualifying_format" VARCHAR(20)
+, "circuit_id" VARCHAR(100) NOT NULL
+, "circuit_type" VARCHAR(6) NOT NULL
 , "course_length" DECIMAL(6,3) NOT NULL
 , "laps" INTEGER NOT NULL
 , "distance" DECIMAL(6,3) NOT NULL
 , "scheduled_laps" INTEGER
 , "scheduled_distance" DECIMAL(6,3)
 , "pre_qualifying_date" DATE
-, "pre_qualifying_time" TEXT
+, "pre_qualifying_time" VARCHAR(5)
 , "free_practice_1_date" DATE
-, "free_practice_1_time" TEXT
+, "free_practice_1_time" VARCHAR(5)
 , "free_practice_2_date" DATE
-, "free_practice_2_time" TEXT
+, "free_practice_2_time" VARCHAR(5)
 , "free_practice_3_date" DATE
-, "free_practice_3_time" TEXT
+, "free_practice_3_time" VARCHAR(5)
 , "free_practice_4_date" DATE
-, "free_practice_4_time" TEXT
+, "free_practice_4_time" VARCHAR(5)
 , "qualifying_1_date" DATE
-, "qualifying_1_time" TEXT
+, "qualifying_1_time" VARCHAR(5)
 , "qualifying_2_date" DATE
-, "qualifying_2_time" TEXT
+, "qualifying_2_time" VARCHAR(5)
 , "qualifying_date" DATE
-, "qualifying_time" TEXT
+, "qualifying_time" VARCHAR(5)
 , "sprint_qualifying_date" DATE
-, "sprint_qualifying_time" TEXT
+, "sprint_qualifying_time" VARCHAR(5)
 , "sprint_race_date" DATE
-, "sprint_race_time" TEXT
+, "sprint_race_time" VARCHAR(5)
 , "warming_up_date" DATE
-, "warming_up_time" TEXT
+, "warming_up_time" VARCHAR(5)
 , CONSTRAINT "race_pk" PRIMARY KEY ("id")
 , CONSTRAINT "race_year_round_uk" UNIQUE ("year", "round")
 , CONSTRAINT "race_year_fk" FOREIGN KEY ("year") REFERENCES "season" ("year")
@@ -391,69 +391,69 @@ CREATE INDEX "race_circuit_id_idx" ON "race" ("circuit_id");
 
 CREATE TABLE "race_data"
 ( "race_id" INTEGER NOT NULL
-, "type" VARCHAR(255) NOT NULL
+, "type" VARCHAR(50) NOT NULL
 , "position_display_order" INTEGER NOT NULL
 , "position_number" INTEGER
-, "position_text" VARCHAR(255)
-, "driver_number" VARCHAR(255) NOT NULL
-, "driver_id" VARCHAR(255) NOT NULL
-, "constructor_id" VARCHAR(255) NOT NULL
-, "engine_manufacturer_id" VARCHAR(255) NOT NULL
-, "tyre_manufacturer_id" VARCHAR(255) NOT NULL
-, "practice_time" VARCHAR(255)
+, "position_text" VARCHAR(4)
+, "driver_number" VARCHAR(3) NOT NULL
+, "driver_id" VARCHAR(100) NOT NULL
+, "constructor_id" VARCHAR(100) NOT NULL
+, "engine_manufacturer_id" VARCHAR(100) NOT NULL
+, "tyre_manufacturer_id" VARCHAR(100) NOT NULL
+, "practice_time" VARCHAR(20)
 , "practice_time_millis" INTEGER
-, "practice_gap" VARCHAR(255)
+, "practice_gap" VARCHAR(20)
 , "practice_gap_millis" INTEGER
-, "practice_interval" VARCHAR(255)
+, "practice_interval" VARCHAR(20)
 , "practice_interval_millis" INTEGER
 , "practice_laps" INTEGER
-, "qualifying_time" VARCHAR(255)
+, "qualifying_time" VARCHAR(20)
 , "qualifying_time_millis" INTEGER
-, "qualifying_q1" VARCHAR(255)
+, "qualifying_q1" VARCHAR(20)
 , "qualifying_q1_millis" INTEGER
-, "qualifying_q2" VARCHAR(255)
+, "qualifying_q2" VARCHAR(20)
 , "qualifying_q2_millis" INTEGER
-, "qualifying_q3" VARCHAR(255)
+, "qualifying_q3" VARCHAR(20)
 , "qualifying_q3_millis" INTEGER
-, "qualifying_gap" VARCHAR(255)
+, "qualifying_gap" VARCHAR(20)
 , "qualifying_gap_millis" INTEGER
-, "qualifying_interval" VARCHAR(255)
+, "qualifying_interval" VARCHAR(20)
 , "qualifying_interval_millis" INTEGER
 , "qualifying_laps" INTEGER
-, "starting_grid_position_grid_penalty" VARCHAR(255)
+, "starting_grid_position_grid_penalty" VARCHAR(20)
 , "starting_grid_position_grid_penalty_positions" INTEGER
-, "starting_grid_position_time" VARCHAR(255)
+, "starting_grid_position_time" VARCHAR(20)
 , "starting_grid_position_time_millis" INTEGER
 , "race_shared_car" BOOLEAN
 , "race_laps" INTEGER
-, "race_time" VARCHAR(255)
+, "race_time" VARCHAR(20)
 , "race_time_millis" INTEGER
-, "race_time_penalty" VARCHAR(255)
+, "race_time_penalty" VARCHAR(20)
 , "race_time_penalty_millis" INTEGER
-, "race_gap" VARCHAR(255)
+, "race_gap" VARCHAR(20)
 , "race_gap_millis" INTEGER
 , "race_gap_laps" INTEGER
-, "race_interval" VARCHAR(255)
+, "race_interval" VARCHAR(20)
 , "race_interval_millis" INTEGER
-, "race_reason_retired" VARCHAR(255)
+, "race_reason_retired" VARCHAR(100)
 , "race_points" DECIMAL(8,2)
 , "race_grid_position_number" INTEGER
-, "race_grid_position_text" VARCHAR(255)
+, "race_grid_position_text" VARCHAR(2)
 , "race_positions_gained" INTEGER
 , "race_pit_stops" INTEGER
 , "race_fastest_lap" BOOLEAN
 , "race_driver_of_the_day" BOOLEAN
 , "race_grand_slam" BOOLEAN
 , "fastest_lap_lap" INTEGER
-, "fastest_lap_time" VARCHAR(255)
+, "fastest_lap_time" VARCHAR(20)
 , "fastest_lap_time_millis" INTEGER
-, "fastest_lap_gap" VARCHAR(255)
+, "fastest_lap_gap" VARCHAR(20)
 , "fastest_lap_gap_millis" INTEGER
-, "fastest_lap_interval" VARCHAR(255)
+, "fastest_lap_interval" VARCHAR(20)
 , "fastest_lap_interval_millis" INTEGER
 , "pit_stop_stop" INTEGER
 , "pit_stop_lap" INTEGER
-, "pit_stop_time" VARCHAR(255)
+, "pit_stop_time" VARCHAR(20)
 , "pit_stop_time_millis" INTEGER
 , "driver_of_the_day_percentage" DECIMAL(4,1)
 , CONSTRAINT "rada_pk" PRIMARY KEY ("race_id", "type", "position_display_order")
@@ -478,8 +478,8 @@ CREATE TABLE "race_driver_standing"
 ( "race_id" INTEGER NOT NULL
 , "position_display_order" INTEGER NOT NULL
 , "position_number" INTEGER
-, "position_text" VARCHAR(255) NOT NULL
-, "driver_id" VARCHAR(255) NOT NULL
+, "position_text" VARCHAR(4) NOT NULL
+, "driver_id" VARCHAR(100) NOT NULL
 , "points" DECIMAL(8,2) NOT NULL
 , "positions_gained" INTEGER
 , CONSTRAINT "rads_pk" PRIMARY KEY ("race_id", "position_display_order")
@@ -497,9 +497,9 @@ CREATE TABLE "race_constructor_standing"
 ( "race_id" INTEGER NOT NULL
 , "position_display_order" INTEGER NOT NULL
 , "position_number" INTEGER
-, "position_text" VARCHAR(255) NOT NULL
-, "constructor_id" VARCHAR(255) NOT NULL
-, "engine_manufacturer_id" VARCHAR(255) NOT NULL
+, "position_text" VARCHAR(4) NOT NULL
+, "constructor_id" VARCHAR(100) NOT NULL
+, "engine_manufacturer_id" VARCHAR(100) NOT NULL
 , "points" DECIMAL(8,2) NOT NULL
 , "positions_gained" INTEGER
 , CONSTRAINT "racs_pk" PRIMARY KEY ("race_id", "position_display_order")
